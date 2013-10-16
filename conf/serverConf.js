472b7a98ce53d5087c58f02446d97f9b462d6c26
lvleibing01 <lvleibing01@baidu.com>
2013-10-17 01:47:34 +0800

    update

--- conf/serverConf.js
+++ conf/serverConf.js
@@ -11,13 +11,15 @@ var conf = {
         m: 8 * 1024 * 1024 * 20,
         n: 1024 * 1024
     },
-    db: {
-        host: '10.46.7.171',
-        port: 4198,
-        user: 'wise_novelclu_w',
-        password: 'C9l3U4n6M2e1',
-        database: 'novels_new'
-    }
+    db: [
+        {
+            host: '10.46.7.171',
+            port: 4198,
+            user: 'wise_novelclu_w',
+            password: 'C9l3U4n6M2e1',
+            database: 'novels_new'
+        }
+    ]
 };
 
 module.exports = conf;
