60a1bea1bf557fce00654b571e668e65274dac98
lvleibing01 <lvleibing01@baidu.com>
2013-10-17 14:54:39 +0800

    update

--- bloomfilter/bloomfilter.js
+++ bloomfilter/bloomfilter.js
@@ -6,6 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var fs = require('fs');
 var util = require('util');
 var BloomFilter = require('bloomfilter').BloomFilter;
 
@@ -17,13 +18,51 @@ function _BloomFilter(conf){
         conf = require('./bloomfilterConfig');
     }
 
-    var m = conf.m;
-    var n = conf.n;
-    var k = Math.ceil( Math.log(2) * m / n);
+    this.m = conf.m;
+    this.n = conf.n;
+    this.k = Math.ceil( Math.log(2) * m / n);
+    this.backFile = conf.backupFile;
+    this.bucketsBackup = new Array();
 
-    BloomFilter.call(this, m, k);
+    this.recover();
+
+    if(this.bucketsBackup.length == 0){
+        BloomFilter.call(this, m, k);
+    }else{
+        BloomFilter.call(this, this.bucketsBackup, k);
+    }
 
 }
 
 util.inherits(_BloomFilter, BloomFilter);
+
+_BloomFilter.prototype.recover = function (){
+
+    fs.readFile(this.backFile, function(err, data){
+
+        if(err){
+           throw err;
+        }
+
+        var obj = JSON.parse(data.toString())
+
+        if(obj){
+
+            if(obj.m === this.m){
+                this.bucketsBackup = obj.buckets;
+            }
+
+        }
+
+    })
+
+};
+
+_BloomFilter.prototype.backup = function (){
+
+    fs.writeFile(this.backFile, JSON.stringify({m: this.m, buckets: this.buckets}), function(err){
+        util.log(util.format('[backup] m: %d, buckets: %d', this.m, this.buckets.length))
+    })
+
+};
 
